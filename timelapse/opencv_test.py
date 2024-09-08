bf44a056e38efd67a3278676db97132f47d8c28d
Julia Mårtensson <juliaam@protonmail.com>
2024-09-08 14:38:26 +0200

    add more recordings and update script

--- timelapse/opencv_test.py
+++ timelapse/opencv_test.py
@@ -1,37 +1,37 @@
 import cv2
 import matplotlib.pyplot as plt
 
-# Öppna videon med hjälp av cv2.VideoCapture
-video_path = "/home/naph/files/uni/kem929/KEM929/timelapse/july/pH3_IS-I_with_straw_2024-07-24.timelapse.mp4"  # Byt ut med sökvägen till din video
+# Open the video with cv2.VideoCapture
+video_path = "/home/naph/files/uni/kem929/KEM929/timelapse/august/pH7_IS-0_with_straw_6mm_2024_08_12.mkv.timelapse.mp4"
 cap = cv2.VideoCapture(video_path)
 
-# Kontrollera om videon öppnades framgångsrikt
+# Control that the video was opened successfully
 if not cap.isOpened():
     print("Kunde inte öppna videon.")
     exit()
 
-# Ange downscaling-faktorn
-scale_factor = 0.5  # Minskar storleken till 50% av originalet
+# Downscaling-faktorn
+scale_factor = 0.5  # Decreasing the size of the video to 50% of the original
 
 def downscale_video():
-    # Läs en bildruta från videon
+    # Read frame from the video
     ret, frame = cap.read()
 
-    # Om läsningen inte lyckades (slutet av videon nått), bryt loopen
+    # If the reading was not successful (end of video reached), break the loop
     if not ret:
         return 1
 
-    # Downscalea bilden
+    # Downscale bilden
     width = int(frame.shape[1] * scale_factor)
     height = int(frame.shape[0] * scale_factor)
     downscaled_frame1 = cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
     downscaled_frame2 = cv2.resize(cv2.resize(frame, (1, 1), interpolation=cv2.INTER_AREA), (40,40))
 
-    # Visa upp den downscalade bilden
+    # Show the downscaled frame
     cv2.imshow('Downscaled Video 1', downscaled_frame1)
     cv2.imshow('Downscaled Video 2', downscaled_frame2)
 
-    # Avbryt om användaren trycker på 'q'
+    # Exit if the user presses 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         return 1
 
@@ -46,7 +46,9 @@ def plot():
 
         image = cv2.resize(frame, (100, 100), interpolation=cv2.INTER_AREA)
         image_16bit = image.astype("float32")
-        grayscale_values.append(image_16bit.mean()/255) #use mean?? viktar alla lika mycket
+        grayscale_values.append(image_16bit.mean()/255)
+        #use mean?? Want to weigh everything equally (mean) or not?
+
         # Convert frame to grayscale
         #gray_frame = cv2.cvtColor(image_16bit, cv2.COLOR_BGR2GRAY)
 
@@ -56,6 +58,7 @@ def plot():
     cap.release()
 
     print("cap released")
+
     # Plot grayscale values over time
     plt.plot(grayscale_values)
     plt.xlabel('Frame Number')
@@ -71,6 +74,6 @@ plot()
 
 
 
-# Frigör videokällan och stäng alla öppna fönster
+# Free the videosource and close all open windows
 cap.release()
 cv2.destroyAllWindows()
